8e0087d2242fb5af625acdb5746cc7faa2e7a4b5
SanthoshiSelvamani <santhoshisana611@gmail.com>
2025-01-16 20:47:51 +0530

    Delete About.jsx

--- About.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function About() {
-  return (
-    <div>Hii I am Santhoshi . <br />I currently purusing my 1st year in MSc.Computer Science.</div>
-  )
-}
